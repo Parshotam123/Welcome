7264b6856b4dd8d80a54da56721ba3f3b65275cb
Parshotam Kumar <parshotampragani52@gmail.com>
2025-07-24 09:38:42 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+
+let welcomeEl = document.getElementById("welcome-el")
+
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+
+let name = "Per Harald Borgen"
+let greeting = "Welcome back "
+
+// Render the welcome message using welcomeEl.innerText
+
+welcomeEl.innerText = greeting + name
+
+// welcomeEl.innerText = welcomeEl.innerText + "👋" /same as given below 
+
+welcomeEl.innerText += "👋" // More simple then above and avoid repetition.
